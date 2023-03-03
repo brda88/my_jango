b70d91784f48bc3873e51aacbb73a186405f18c1
User <plaksievai@gmail.com>
2023-03-03 16:59:56 +0200

    add words count

--- my_django/views.py
+++ my_django/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    original_text = request.GET['usertext']
+    reverse_text = original_text[::-1]
+    num_of_words = len(original_text.split())
+
+    return render(request, 'reverse.html', {'reversetext': reverse_text,
+                                            'originaltext': original_text,
+                                            'numwords': num_of_words})
+
